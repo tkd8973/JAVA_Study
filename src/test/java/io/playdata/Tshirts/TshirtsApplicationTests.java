8773d1a01c50ab23a8f4890f523ab0851834a5d4
SeoSangwon <126433959+tkd8973@users.noreply.github.com>
2023-05-09 09:28:28 +0900

    Initial commit

---
+++ src/test/java/io/playdata/Tshirts/TshirtsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.playdata.Tshirts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TshirtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
